3319705c8de285f994fb4f145ba75422a02dfcb7
xiangru <xiangru.qian@gmail.com>
2018-10-27 15:32:41 -0400

    LFU function

--- helpers.c
+++ helpers.c
@@ -1,9 +1,14 @@
-//
-// Created by xiangru on 10/24/18.
-//
+typedef struct LFU_entry{
+    char* url;
+    char* body;
+    int freq;
+} lfu_entry;
 
-#include <string.h>
-#include <stdio.h>
+typedef struct Record{
+    char* url;
+    int freq;
+} record;
+
 
 void get_host_ip_and_port(char *url, char *addr, char* port){
     int flag = 0, addr_start = -1, i;
@@ -56,4 +61,53 @@ void get_filename(char *url, char *filename){
         s = url;
     s = strchr(s, '/');
     strcpy(filename, s);
+}
+
+
+char* update_LFU(char *url, char *body, record *rec_table, lfu_entry *lfu, int *rec_tb_len){
+    /**
+     * Update LFU:
+     * 1. If the given URL appear in the cache, then just update the frequency adding 1
+     * 2. Otherwise, if the URL appeared before, increase its frequency by 1 and then check
+     *    whether it is frequent enough to be in the cache. If so, update cache.
+     * 3. If this is the first time this URL appears, add the URL to the record table with
+     *    frequency 1.
+     * In any of the cases, return the body (web content associated with the URL)
+     */
+    int min_entry = 0;
+    for (int i =0; i< 3; i++){
+        if (lfu[i].freq == 0){
+            lfu[i].url = url;
+            lfu[i].freq = 1;
+            lfu[i].body = body;
+            return body;
+        }
+        else {
+            if (strcmp(lfu[i].url, url) == 0) {
+                lfu[i].freq ++;
+                return body;
+            }
+            if (lfu[i].freq < lfu[min_entry].freq){
+                min_entry = i;
+            }
+        }
+    }
+    for (int i = 0; i < *rec_tb_len; i++){
+        if (strcmp(rec_table[i].url, url) == 0) {
+            rec_table[i].freq++;
+            if (rec_table[i].freq >= lfu[min_entry].freq) {
+                int tmp = rec_table[i].freq;
+                rec_table[i].freq = lfu[min_entry].freq;
+                rec_table[i].url = lfu[min_entry].url;
+                lfu[min_entry].url = url;
+                lfu[min_entry].freq = tmp;
+                lfu[min_entry].body = body;
+            }
+            return body;
+        }
+    }
+    rec_table[*rec_tb_len].url = url;
+    rec_table[*rec_tb_len].freq = 1;
+    *rec_tb_len = *rec_tb_len + 1;
+    return body;
 }
